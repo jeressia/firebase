6909bd920c1c02a23d05d33023b1f82ae7b7d300
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-05-20 19:42:35 -0500

    firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,4 @@
-import 'bootstrap';
+import firebase from 'firebase/app';
 
 import auth from './components/auth/auth';
 import birfday from './components/birfday/birfday';
@@ -6,6 +6,7 @@ import birfday from './components/birfday/birfday';
 import '../styles/main.scss';
 
 const init = () => {
+  firebase.initializeApp(firebaseConfig);
   auth.authBuilder();
   birfday.birfdayBuilder();
 };
