1ea26341ae975389650f308ff2949c657de1a915
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-05-20 19:55:27 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,10 +3,13 @@ import firebase from 'firebase/app';
 import auth from './components/auth/auth';
 import birfday from './components/birfday/birfday';
 
+import apiKeys from './helpers/apiKeys.json';
+
 import '../styles/main.scss';
 
 const init = () => {
-  firebase.initializeApp(firebaseConfig);
+  console.error('keys', apiKeys.firebaseKeys);
+  firebase.initializeApp(apiKeys.firebaseKeys);
   auth.authBuilder();
   birfday.birfdayBuilder();
 };
