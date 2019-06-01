e17f6ec6770bd5da13aa35609db01fb4931f45dd
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-06-01 09:52:34 -0500

    friends show

--- src/javascripts/components/birfday/birfday.js
+++ src/javascripts/components/birfday/birfday.js
@@ -5,9 +5,9 @@ import util from '../../helpers/util';
 const birfdayBuilder = (uid) => {
   birfdayData.getBirfdayByUid(uid).then((birthday) => {
     let domString = '<div class="birfdayDiv">';
-    domString += `<h1>${birthday.date}</h1>`;
-    domString += `<img src=${birthday.imageUrl} alt="birthday location"/>`;
-    domString += `<h3>${birthday.location} @ ${birthday.time}</h3>`;
+    domString += `<h1 class="bdate">${birthday.date}</h1>`;
+    domString += `<img src=${birthday.imageUrl} alt="birthday location" class="img-thumbnail"/>`;
+    domString += `<h3 class="blocate">${birthday.location} @ ${birthday.time}</h3>`;
     domString += '</div>';
     util.printToDom('event', domString);
   }).catch(err => console.error('could not get birfday', err));
--- src/javascripts/components/friends/friends.js
+++ src/javascripts/components/friends/friends.js
@@ -31,10 +31,22 @@ const newFriendButton = () => {
   document.getElementById('saveNewFriend').addEventListener('click', createNewFriend);
 };
 
-const showFriends = () => {
-  const domString = '<button id="add-friend-btn" class="btn btn-secondary">Add Friend</button>';
+const showFriends = (friends) => {
+  let domString = '<button id="add-friend-btn" class="btn btn-info">Add Friend</button>';
+  friends.forEach((friend) => {
+    domString += `<h3>${friend.name}`;
+  });
   util.printToDom('friends', domString);
   document.getElementById('add-friend-btn').addEventListener('click', newFriendButton);
 };
 
-export default { showFriends };
+const getFriends = (uid) => {
+  friendsData.getFriendsByUid(uid)
+    .then((friends) => {
+      console.error('friends array', friends);
+      showFriends(friends);
+    })
+    .catch(err => console.error('no friends', err));
+};
+
+export default { getFriends };
